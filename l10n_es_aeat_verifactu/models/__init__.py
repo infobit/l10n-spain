f1ccb52138c92bf8306d62bbc71f753bfcc24ca6
root <nazaret@infobit.es>
2025-05-22 17:45:17 +0200

    [NEW] Verifactu

---
+++ l10n_es_aeat_verifactu/models/__init__.py
@@ -0,0 +1,11 @@
+import aeat_verifactu_map
+import aeat_verifactu_map_lines
+import aeat_verifactu_registration_keys
+import certificate_verifactu
+import account_journal
+import res_company
+from . import account_invoice_line
+from . import account_invoice
+from . import aeat_tax_agency
+import account_fiscal_position
+import res_partner
